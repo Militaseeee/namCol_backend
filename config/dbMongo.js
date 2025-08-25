dc4afa83742652480baf3b9c236a0973ef6e3254
Militaseeee <camilitaacosta2001@gmail.com>
2025-08-25 06:55:05 -0500

    feat: update info conex BD

--- config/dbMongo.js
+++ config/dbMongo.js
@@ -7,7 +7,7 @@ export const dbConnection = async () => {
   try {
     const mongodbAtlas = process.env.DB_MONGO;
     await mongoose.connect(mongodbAtlas);
-    console.log("✅ Conectado a MongoDB Atlas");
+    console.log("✅ Connected to MongoDB Atlas");
   } catch (error) {
     console.error("❌ Database error", error);
     process.exit(1);
